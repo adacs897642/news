94928474e49b48ea7a1ab6d99b9c9c8f5eb9c1ea
adacs897642 <adacs897642@gmail.com>
2016-02-05 14:44:23 +0300

    add class

--- Models/article.php
+++ Models/article.php
@@ -1,17 +1,23 @@
 <?php
 
-require __DIR__ . '/../functions/functions.php';
+require_once __DIR__ . '/../functions/myDB.php';
+
+
 
 function Article_get(){
 
+    $newDB = new myDB('news');
+
     $query = "SELECT id, title, annotation, time FROM article ORDER BY TIME DESC";
 
-    return sendQuery($query);
+    return $newDB->select($query);
 }
 
 function Article_getOnce($id){
 
+    $newDB = new myDB('news');
+
     $query = "SELECT title, annotation, time, text FROM article WHERE id=$id";
 
-    return sendQuery($query);
+    return $newDB->select($query);
 }
--- functions/myDB.php
+++ functions/myDB.php
@@ -6,7 +6,64 @@
  * Date: 04.02.2016
  * Time: 16:16
  */
-class myDB
+abstract class dataBase
 {
+    protected $server;
+    protected $user;
+    protected $pass;
+    protected $dbName;
 
+    abstract protected function connect();
+    abstract protected function sendQuery($sql);
+
+}
+
+class myDB extends dataBase{
+
+    protected function connect()
+    {
+        // TODO: Implement connect() method.
+
+        mysql_connect($this->server,$this->user,$this->pass);
+        mysql_select_db($this->dbName);
+    }
+
+    function __construct($dbName, $server='127.0.0.1',$user='root',$pass='')
+    {
+        $this->server = $server;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->dbName = $dbName;
+        $this->connect();
+    }
+    protected function sendQuery($sql)
+    {
+        // TODO: Implement sendQuery() method.
+        return mysql_query($sql);
+
+    }
+
+    public function insert($date)
+    {
+        // TODO: Implement insert() method.
+        return $this->sendQuery($date);
+    }
+
+    public function update($date)
+    {
+        // TODO: Implement update() method.
+        return $this->sendQuery($date);
+    }
+
+    public function select($data)
+    {
+        // TODO: Implement select() method.
+        $res = $this->sendQuery($data);
+        $arr = [];
+        while($row = mysql_fetch_assoc($res)){
+
+            $arr[] = $row;
+        }
+        return $arr;
+    }
 }
